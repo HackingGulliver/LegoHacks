fdb319455edf0c10b8d4dd09f01c775a1b2ef7ca
Thomas@Gandalf <t.schweitzer@gmx.de>
2016-04-09 19:28:44 +0200

    Shiftregister wird mittels SPI angesteuert

--- software/arduino/Workspace/SerialDriver/SerialDriver.cpp
+++ software/arduino/Workspace/SerialDriver/SerialDriver.cpp
@@ -2,123 +2,33 @@
 #include "SerialDriver.h"
 #include "PWMStateCalculator.h"
 
-#define SHIFT_DATA_PIN 4
-#define SHIFT_CLOCK_PIN 3
+#include <SPI.h>
+
+#define SHIFT_DATA_PIN 11
+#define SHIFT_CLOCK_PIN 13
 #define SHIFT_STORE_PIN 2
 
 PWMStateCalculator pwmStateCalculator;
 
-void setup()
-{
-	pinMode(SHIFT_DATA_PIN, OUTPUT);
-	pinMode(SHIFT_CLOCK_PIN, OUTPUT);
+void setup() {
+
 	pinMode(SHIFT_STORE_PIN, OUTPUT);
 
+	SPI.begin();
+	SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE0));
 }
 
-//void writeToShift(uint8_t value) {
-//	uint8_t oldSREG = SREG;
-//	cli();
-//
-//	// Clock (3): PORTD = 8
-//	// Store (2): PORTD = 4
-//	// Data (4): PORTD = 16
-//
-//	uint8_t i = 8;
-//	do {
-//		if (value & 1) {
-//			PORTD = 16; // data = 1, store = 0;
-//			PORTD = 24; // data = 1, clock = 1;
-//		} else {
-//			PORTD = 0;// data = 0, store = 0
-//			PORTD = 8; // data = 0, clock = 1
-//		}
-//		value >>= 1;
-//		--i;
-//	} while (i);
-//	PORTD = 4; // store = 1
-//	SREG = oldSREG;
-//
-//}
+
 void writeToShift(uint8_t value, boolean finish) {
 #ifndef DEBUG
-	uint8_t oldSREG = SREG;
-	cli();
-
-
-	// Clock (3): PORTD = 8
-	// Store (2): PORTD = 4
-	// Data (4): PORTD = 16
-
-	if (value & 1) {
-		PORTD = 16; // data = 1, store = 0;
-		PORTD = 24; // data = 1, clock = 1;
-	} else {
-		PORTD = 0;// data = 0, store = 0
-		PORTD = 8; // data = 0, clock = 1
-	}
-
-	if (value & 2) {
-		PORTD = 16; // data = 1, store = 0;
-		PORTD = 24; // data = 1, clock = 1;
-	} else {
-		PORTD = 0;// data = 0, store = 0
-		PORTD = 8; // data = 0, clock = 1
-	}
-
-	if (value & 4) {
-		PORTD = 16; // data = 1, store = 0;
-		PORTD = 24; // data = 1, clock = 1;
-	} else {
-		PORTD = 0;// data = 0, store = 0
-		PORTD = 8; // data = 0, clock = 1
-	}
-
-	if (value & 8) {
-		PORTD = 16; // data = 1, store = 0;
-		PORTD = 24; // data = 1, clock = 1;
-	} else {
-		PORTD = 0;// data = 0, store = 0
-		PORTD = 8; // data = 0, clock = 1
-	}
-
-	if (value & 16) {
-		PORTD = 16; // data = 1, store = 0;
-		PORTD = 24; // data = 1, clock = 1;
-	} else {
-		PORTD = 0;// data = 0, store = 0
-		PORTD = 8; // data = 0, clock = 1
-	}
-
-	if (value & 32) {
-		PORTD = 16; // data = 1, store = 0;
-		PORTD = 24; // data = 1, clock = 1;
-	} else {
-		PORTD = 0;// data = 0, store = 0
-		PORTD = 8; // data = 0, clock = 1
-	}
-
-	if (value & 64) {
-		PORTD = 16; // data = 1, store = 0;
-		PORTD = 24; // data = 1, clock = 1;
-	} else {
-		PORTD = 0;// data = 0, store = 0
-		PORTD = 8; // data = 0, clock = 1
-	}
-
-	if (value & 128) {
-		PORTD = 16; // data = 1, store = 0;
-		PORTD = 24; // data = 1, clock = 1;
-	} else {
-		PORTD = 0;// data = 0, store = 0
-		PORTD = 8; // data = 0, clock = 1
-	}
+	PORTD = 0;
+	SPI.transfer(value);
+	PORTD = 4;
 
 	if (finish) {
 		PORTD = 4; // store = 1
 	}
 
-	SREG = oldSREG;
 #endif
 }
 
