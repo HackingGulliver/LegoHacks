7d30f2d338e776ce911c6258ff3fa968691c391d
Thomas@Gandalf <t.schweitzer@gmx.de>
2016-04-13 17:21:56 +0200

    Benchmarks zur Auswertung der Last durch die PWM-Timer erweitert.

--- software/arduino/Workspace/SerialDriver/SerialDriver.cpp
+++ software/arduino/Workspace/SerialDriver/SerialDriver.cpp
@@ -34,7 +34,7 @@ void setup() {
 
 	Timer1.initialize();
 	Timer1.setPeriod(MICROSEC_PER_AT_100HZ);
-	Timer1.attachInterrupt(performPWMCycle);
+//	Timer1.attachInterrupt(performPWMCycle);
 
 	Serial.begin(57600);
 }
@@ -73,16 +73,16 @@ void pwm(uint8_t duty, uint8_t value) {
 void performPWMCycle() {
 	uint8_t state;
 	bool change1 = pwmStateCalculator1.tickAndTest();
-//	bool change2 = pwmStateCalculator2.tickAndTest();
-//	bool change3 = pwmStateCalculator3.tickAndTest();
-//	bool change4 = pwmStateCalculator4.tickAndTest();
-	if (change1) {// || change2 || change3 || change4) {
-//		state = pwmStateCalculator4.nextState();
-//		writeToShift(state, false);
-//		state = pwmStateCalculator3.nextState();
-//		writeToShift(state, false);
-//		state = pwmStateCalculator2.nextState();
-//		writeToShift(state, false);
+	bool change2 = pwmStateCalculator2.tickAndTest();
+	bool change3 = pwmStateCalculator3.tickAndTest();
+	bool change4 = pwmStateCalculator4.tickAndTest();
+	if (change1 || change2 || change3 || change4) {
+		state = pwmStateCalculator4.nextState();
+		writeToShift(state, false);
+		state = pwmStateCalculator3.nextState();
+		writeToShift(state, false);
+		state = pwmStateCalculator2.nextState();
+		writeToShift(state, false);
 		state = pwmStateCalculator1.nextState();
 		writeToShift(state, true);
 	}
@@ -127,7 +127,7 @@ void showChaserLight() {
 	}
 }
 
-void benchmark() {
+uint32_t benchmark() {
 	//	showChaserLight();
 	uint32_t counter = 0;
 	unsigned long inOneSecond = millis() + 1000l;
@@ -136,6 +136,7 @@ void benchmark() {
 		counter++;
 	}
 	Serial.println(counter);
+	return counter;
 }
 
 // Profile: Eine Sekunde Counter hoch zählen und dabei die Zeit für die Sekunde vergleichen
@@ -161,20 +162,45 @@ void benchmark() {
 // 470Hz: 115000
 // 535Hz: 63000
 
+void emptyFunction() {
+
+}
+
 // The loop function is called in an endless loop
 void loop()
 {
+	// Benchmark without timer
+	uint32_t noTimer = benchmark();
+
+	// Benchmark with empty timer
+	Timer1.attachInterrupt(emptyFunction);
+	uint32_t emptyTimer = benchmark();
+
+	// Benchmark with PWM timer
+	Timer1.detachInterrupt();
+	Timer1.attachInterrupt(performPWMCycle);
+
 	showRisingBrightness(0, pwmStateCalculator1);
 	showRisingBrightness(20, pwmStateCalculator2);
 	showRisingBrightness(40, pwmStateCalculator3);
 	showRisingBrightness(60, pwmStateCalculator4);
-	benchmark();
+	uint32_t differentPWM = benchmark();
+
 	showBrightness(10, pwmStateCalculator1);
-	benchmark();
+	uint32_t samePWM = benchmark();
 	showBrightness(5, pwmStateCalculator1);
-	benchmark();
+	samePWM += benchmark();
 	showBrightness(0, pwmStateCalculator1);
-	benchmark();
+	samePWM += benchmark();
+
+	Serial.println();
+	Serial.print("Leerer Timer: ");
+	Serial.println((float) emptyTimer / noTimer);
+	Serial.print("Unterschiedliche PWMs: ");
+	Serial.println((float) differentPWM / noTimer);
+	Serial.print("Gleiche PWMs: ");
+	Serial.println((float) samePWM / (3 * noTimer));
+	Serial.println();
 
 	showChaserLight();
 
