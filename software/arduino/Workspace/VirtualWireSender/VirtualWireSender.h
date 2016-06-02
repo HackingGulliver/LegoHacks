91a292b2f9e3cbae92a1e9330118fcdaaa60cf68
Thomas@Gandalf <t.schweitzer@gmx.de>
2016-06-02 21:47:35 +0200

    Neue Projekte zum Experimentieren mit der VirtualWire-Lib hinzugefügt. Senden und Empfangen mit dem mitgelieferten Beispielcode ist damit möglich.

---
+++ software/arduino/Workspace/VirtualWireSender/VirtualWireSender.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _VirtualWireSender_H_
+#define _VirtualWireSender_H_
+#include "Arduino.h"
+//add your includes for the project VirtualWireSender here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project VirtualWireSender here
+
+
+
+
+//Do not add code below this line
+#endif /* _VirtualWireSender_H_ */
