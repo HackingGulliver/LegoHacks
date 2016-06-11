84b38e71e7fe80d629784f08a86425d1d163debe
Thomas@Gandalf <t.schweitzer@gmx.de>
2016-06-11 21:37:52 +0200

    VirtualWireReceiver gibt aus, wieviele Nachrichten pro Sekunden empfangen wurden.

--- software/arduino/Workspace/VirtualWireReceiver/VirtualWireReceiver.cpp
+++ software/arduino/Workspace/VirtualWireReceiver/VirtualWireReceiver.cpp
@@ -3,12 +3,19 @@
 
 #include "VirtualWire.h"
 
+uint32_t start = millis();
+uint32_t messages = 0;
+
 void setup()
 {
-    Serial.begin(9600);	// Debugging only
+    Serial.begin(57600);	// Debugging only
     Serial.println("setup");
 
     // Initialise the IO and ISR
+    vw_set_rx_pin(12);
+    vw_set_tx_pin(10);
+    vw_set_ptt_pin(9);
+
     vw_set_ptt_inverted(true); // Required for DR3100
     vw_setup(2000);	 // Bits per sec
 
@@ -24,16 +31,22 @@ void loop()
     {
 	int i;
 
-        digitalWrite(13, true); // Flash a light to show received good message
+		++messages;
+		uint32_t duration = millis() - start;
+		uint32_t msgPerSec = messages * 10000 / duration;
+		Serial.print(msgPerSec);
+//        digitalWrite(13, true); // Flash a light to show received good message
 	// Message with a good checksum received, dump it.
-	Serial.print("Got: ");
+//	Serial.print("Got: ");
+//
+//	for (i = 0; i < buflen; i++)
+//	{
+//	    Serial.print(buf[i], HEX);
+//	    Serial.print(" ");
+//	}
+//	Serial.println("");
+    Serial.print(".");
 
-	for (i = 0; i < buflen; i++)
-	{
-	    Serial.print(buf[i], HEX);
-	    Serial.print(" ");
-	}
-	Serial.println("");
-        digitalWrite(13, false);
+//        digitalWrite(13, false);
     }
 }
